25b852b99829251416e390e9c84099e5b9ee514e
togoai0930@icloud.com <togoai0930@icloud.com>
2024-10-03 13:55:40 +0900

    抹消；；

--- src/app/components/Score/types.ts
+++
@@ -1,6 +0,0 @@
-// プレイヤー情報を格納するための型
-export interface Player {
-  id: number;
-  name: string;
-  score: number; // 持ち点
-}
--- src/app/data/yakuData.ts
+++
@@ -1,8 +0,0 @@
-// 役のデータを配列にする
-export const yakuData = [
-  { id: 1, name: "ピンフ", han: 1 },
-  { id: 2, name: "タンヤオ", han: 1 },
-  { id: 3, name: "三色同順", han: 2 },
-  { id: 4, name: "四暗刻", han: 3 },
-  { id: 5, name: "大三元", han: 3 },
-];
